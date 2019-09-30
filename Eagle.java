818c92e19f2a180e3c0910152f9a79081ead11da
52-28 <aymeric.feillault@gmail.com>
2019-09-30 09:44:53 +0200

    resolution

--- Eagle.java
+++ Eagle.java
@@ -1,12 +1,13 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
 
     public Eagle(String name) {
         super(name);
-        this.flying = false;
+        this.flying = true;
         this.altitude = 0;
+	
     }
 
     public int getAltitude() {
@@ -21,4 +22,46 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+
+    @Override
+    public void takeOff() {
+        if (this.flying) {
+            System.out.printf("%s can takeoff %n", this.getName());
+	}
+    }
+
+    @Override
+    public int ascend(int altitude) {
+        if (this.flying) {
+	    this.altitude = altitude;
+            System.out.printf("%s ascend over: %d%n", this.getName(), this.altitude);
+	}
+	return this.altitude;
+    }
+
+    @Override
+    public int descend(int altitude) {
+	this.altitude = altitude;
+        if (this.flying && this.altitude != 0) {
+            System.out.printf("%s can go down to dive to hunt from %d meters %n", this.getName(), this.altitude);
+	}
+	else {
+            System.out.printf("%s lives on trees%n", this.getName());	
+	}
+	return this.altitude;
+    }
+
+    @Override
+    public void land() {
+	this.altitude = altitude;
+        if (!this.flying) {
+            System.out.printf("%s can't fly, it's living in plains%n", this.getName());
+	}
+	else {
+            System.out.printf("%s walks trough plains all along the day%n", this.getName());	
+	}
+    }
+
+
 }
